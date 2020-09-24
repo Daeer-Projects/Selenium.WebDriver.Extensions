5ba95c4f6750fdd9034ee2fe4636f11d7e286f60
MADDY\daeer <daeer73@gmail.com>
2020-09-24 14:52:44 +0100

    Added the `SafeFindElement` and `SafeFindElements` methods
    Added the "find" version of the methods to fix the issue raised on GitHub.

--- tests/WebDriver.Extension.Tests/WebDriverExtensionTests.cs
+++ tests/WebDriver.Extension.Tests/WebDriverExtensionTests.cs
@@ -511,5 +511,488 @@ namespace WebDriver.Extension.Tests
         }
 
         #endregion Safe Get Elements
+
+        #region Safe Find Element
+
+        [Fact]
+        public void Test_safe_find_with_null_parameters_returns_null()
+        {
+            // Arrange.
+            // Act.
+            var result = _driver.SafeFindElement(null);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_null_function_returns_null()
+        {
+            // Arrange.
+            // Act.
+            var result = _driver.SafeFindElement(null, 4);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_null_by_returns_null()
+        {
+            // Arrange.
+            // Act.
+            var result = _driver.SafeFindElement(null, 1, (element) => element);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_null_timeout_and_function_and_element_not_found_returns_null()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).ReturnsNull();
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"));
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_zero_timeout_and_element_not_found_returns_null()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).ReturnsNull();
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"), 0, (element) => element.Displayed ? element : null);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_two_timeout_and_element_not_found_with_function_returns_null()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).ReturnsNull();
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"), 2, (element) => element.Displayed ? element : null);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_null_function_and_element_found_returns_element()
+        {
+            // Arrange.
+            _element.Displayed.Returns(true);
+            _driver.FindElement(Arg.Any<By>()).Returns(_element);
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"), 5);
+
+            // Assert.
+            result.Should().Be(_element);
+        }
+
+        [Fact]
+        public void Test_safe_find_with_five_timeout_and_element_found_returns_element()
+        {
+            // Arrange.
+            _element.Displayed.Returns(true);
+            _driver.FindElement(Arg.Any<By>()).Returns(_element);
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"), 5, (element) => element.Displayed ? element : null);
+
+            // Assert.
+            result.Should().Be(_element);
+        }
+
+        [Fact]
+        public void Test_safe_find_with_five_timeout_and_null_function_element_found_returns_element_before_time_out_has_passed()
+        {
+            // Arrange.
+            _element.Displayed.Returns(true);
+            _driver.FindElement(Arg.Any<By>()).Returns(_element);
+            var stopWatch = new Stopwatch();
+
+            // Act.
+            stopWatch.Start();
+            var result = _driver.SafeFindElement(By.Id("banana"), 5);
+            stopWatch.Stop();
+
+            // Assert.
+            result.Should().Be(_element);
+            stopWatch.Elapsed.Seconds.Should().BeLessThan(5);
+        }
+
+        [Fact]
+        public void Test_safe_find_with_five_timeout_and_element_found_returns_element_before_time_out_has_passed()
+        {
+            // Arrange.
+            _element.Displayed.Returns(true);
+            _driver.FindElement(Arg.Any<By>()).Returns(_element);
+            var stopWatch = new Stopwatch();
+
+            // Act.
+            stopWatch.Start();
+            var result = _driver.SafeFindElement(By.Id("banana"), 5, (element) => element.Displayed ? element : null);
+            stopWatch.Stop();
+
+            // Assert.
+            result.Should().Be(_element);
+            stopWatch.Elapsed.Seconds.Should().BeLessThan(5);
+        }
+
+        [Fact]
+        public void Test_safe_find_with_zero_timeout_and_null_function_and_no_such_element_exception_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).Throws(new NoSuchElementException());
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"));
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_zero_timeout_and_null_function_and_no_such_frame_exceptions_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).Throws(new NoSuchFrameException());
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"));
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_zero_timeout_and_null_function_and_no_such_window_exceptions_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).Throws(new NoSuchWindowException());
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"));
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_zero_timeout_and_null_function_and_exceptions_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).Throws(new Exception());
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"));
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_one_timeout_and_null_function_and_no_such_element_exception_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).Throws(new NoSuchElementException());
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"), 1);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_one_timeout_and_null_function_and_no_such_frame_exceptions_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).Throws(new NoSuchFrameException());
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"), 1);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_one_timeout_and_null_function_and_no_such_window_exceptions_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).Throws(new NoSuchWindowException());
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"), 1);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_one_timeout_and_function_and_no_such_element_exception_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).Throws(new NoSuchElementException());
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"), 1, (element) => element);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_one_timeout_and_function_and_no_such_frame_exceptions_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).Throws(new NoSuchFrameException());
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"), 1, (element) => element);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_one_timeout_and_function_and_no_such_window_exceptions_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).Throws(new NoSuchWindowException());
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"), 1, (element) => element);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_with_three_second_timeout_and_element_not_found_returns_null_after_time_out()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).Throws(new NoSuchElementException());
+            var stopWatch = new Stopwatch();
+
+            // Act.
+            stopWatch.Start();
+            var result = _driver.SafeFindElement(By.Id("banana"), 3, (element) => element.Displayed ? element : null);
+            stopWatch.Stop();
+
+            // Assert.
+            result.Should().BeNull();
+            stopWatch.Elapsed.TotalMilliseconds.Should().BeInRange(2500.0, 3500.0);
+        }
+
+        [Fact]
+        public void Test_safe_find_with_zero_timeout_and_exception_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElement(Arg.Any<By>()).Throws(new NoSuchElementException());
+
+            // Act.
+            var result = _driver.SafeFindElement(By.Id("banana"), 0, (element) => element.Displayed ? element : null);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        #endregion Safe Find Element
+
+        #region Safe Find Elements
+
+        [Fact]
+        public void Test_safe_find_elements_with_null_parameters_returns_null()
+        {
+            // Arrange.
+            // Act.
+            var result = _driver.SafeFindElements(null);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_elements_with_timeout_returns_null()
+        {
+            // Arrange.
+            // Act.
+            var result = _driver.SafeFindElements(null, 4);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_elements_with_null_timeout_and_elements_not_found_returns_null()
+        {
+            // Arrange.
+            _driver.FindElements(Arg.Any<By>()).ReturnsNull();
+
+            // Act.
+            var result = _driver.SafeFindElements(By.Id("banana"));
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_elements_with_zero_timeout_and_element_not_found_returns_null()
+        {
+            // Arrange.
+            _driver.FindElements(Arg.Any<By>()).ReturnsNull();
+
+            // Act.
+            var result = _driver.SafeFindElements(By.Id("banana"), 0);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_elements_with_time_out_and_element_found_returns_element()
+        {
+            // Arrange.
+            var expected = new List<IWebElement>
+            {
+                _element
+            };
+
+            _driver.FindElements(Arg.Any<By>()).Returns(expected.AsReadOnly());
+
+            // Act.
+            var result = _driver.SafeFindElements(By.Id("banana"), 5);
+
+            // Assert.
+            result.Should().BeEquivalentTo(expected);
+        }
+
+        [Fact]
+        public void Test_safe_find_elements_with_five_timeout_and_null_function_element_found_returns_element_before_time_out_has_passed()
+        {
+            // Arrange.
+            var expected = new List<IWebElement>
+            {
+                _element
+            };
+
+            _driver.FindElements(Arg.Any<By>()).Returns(expected.AsReadOnly());
+            var stopWatch = new Stopwatch();
+
+            // Act.
+            stopWatch.Start();
+            var result = _driver.SafeFindElements(By.Id("banana"), 5);
+            stopWatch.Stop();
+
+            // Assert.
+            result.Should().BeEquivalentTo(expected);
+            stopWatch.Elapsed.Seconds.Should().BeLessThan(5);
+        }
+
+        [Fact]
+        public void Test_safe_find_elements_with_zero_timeout_and_null_function_and_no_such_element_exception_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElements(Arg.Any<By>()).Throws(new NoSuchElementException());
+
+            // Act.
+            var result = _driver.SafeFindElements(By.Id("banana"));
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_elements_with_zero_timeout_and_null_function_and_no_such_frame_exceptions_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElements(Arg.Any<By>()).Throws(new NoSuchFrameException());
+
+            // Act.
+            var result = _driver.SafeFindElements(By.Id("banana"));
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_elements_with_zero_timeout_and_null_function_and_no_such_window_exceptions_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElements(Arg.Any<By>()).Throws(new NoSuchWindowException());
+
+            // Act.
+            var result = _driver.SafeFindElements(By.Id("banana"));
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_elements_with_zero_timeout_and_null_function_and_exceptions_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElements(Arg.Any<By>()).Throws(new Exception());
+
+            // Act.
+            var result = _driver.SafeFindElements(By.Id("banana"));
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_elements_with_one_timeout_and_null_function_and_no_such_element_exception_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElements(Arg.Any<By>()).Throws(new NoSuchElementException());
+
+            // Act.
+            var result = _driver.SafeFindElements(By.Id("banana"), 1);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_elements_with_one_timeout_and_null_function_and_no_such_frame_exceptions_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElements(Arg.Any<By>()).Throws(new NoSuchFrameException());
+
+            // Act.
+            var result = _driver.SafeFindElements(By.Id("banana"), 1);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        [Fact]
+        public void Test_safe_find_elements_with_one_timeout_and_null_function_and_no_such_window_exceptions_thrown_returns_null()
+        {
+            // Arrange.
+            _driver.FindElements(Arg.Any<By>()).Throws(new NoSuchWindowException());
+
+            // Act.
+            var result = _driver.SafeFindElements(By.Id("banana"), 1);
+
+            // Assert.
+            result.Should().BeNull();
+        }
+
+        #endregion Safe Find Elements
     }
 }
